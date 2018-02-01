090fea2994ce0c07420fa3df466b71fb6aee70bd
Vincent <vincentmohr0909@gmail.com>
2018-02-01 12:45:50 +0100

    SpielStand

---
+++ FussballSpiel.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class FussballSpiel {
+
+
+    public static void main (int[]args){
+
+
+        ArrayList listeTore = new ArrayList();
+        ArrayList<Integer[]> list = new ArrayList();
+        listeTore.add("-" + Toreheimmannschaften,  "  -" +ToreGastmannschaft);
+        listeTore.remove(Toreheimmannschaften , ToreGastmannschaft);
+
+    }
+
+}
